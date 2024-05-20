712c2dd13bd915f452b0012cd9347bdad119ab4c
ayoubdkhissi <ayoub.dkhissi@outlook.com>
2024-05-20 11:22:14 +0100

    Add appsettings file

--- Program.cs
+++ Program.cs
@@ -3,9 +3,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using pokemon_portal_blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Configuration.AddJsonFile("appsettings.json");
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(Environment.GetEnvironmentVariable("API_BASE_URL")!) });
+
 
 await builder.Build().RunAsync();
